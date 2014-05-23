ba93a77473866af251b1c880f36ff33aa4f9b7a9
antmachine <anthonysallows@gmail.com>
2014-05-23 15:57:45 -0700

    copied AJAX Map lab for pins_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523224154) do
+ActiveRecord::Schema.define(version: 20140523225040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140523224154) do
     t.datetime "updated_at"
   end
 
-  create_table "maps", force: true do |t|
+  create_table "pins", force: true do |t|
     t.float    "latitude"
     t.float    "longitude"
     t.datetime "created_at"
