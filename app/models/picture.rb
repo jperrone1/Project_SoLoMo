e2ae650385428b40c67a068b98a94b54c7dd7ad9
Jeffrey Perrone <jperrone@well.com>
2014-05-26 15:52:48 -0700

    Added Paperclip changes.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,16 @@
 class Picture < ActiveRecord::Base
+
+  # This method associates the attribute ":image" with a file attachment
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  # Create ActiveRecord methods to facilitate data retrieval:
+  belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526223245) do
+ActiveRecord::Schema.define(version: 20140526223814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 20140526223245) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "posts", force: true do |t|
