707bc898201feb04cfddd8dc972ae30c6b83b5d7
flanagan <flanagan9@gmail.com>
2014-05-27 09:18:07 -0700

    jquery delete item working, not posting to DB yet

--- app/assets/javascripts/new_post.js
+++ app/assets/javascripts/new_post.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
    var loadPosts = function() {
       $.ajax('/posts.json', {type: 'get'}).success(function(data){
         for (var i in data) {
+            console.log("Hello Retreived!");
+
           $('.post_1').append('<li id=' + data[i]["id"] + '>' + data[i]["title"] + ' - ' + data[i]["body"] + ' ' + '<button class="delete" type="submit">' + 'Delete </button>' + '<button class="show" type="submit">' + 'Show </button>'+'</li>' );
         }
       });
     }
-    loadPosts();
 
+  //saves to database
   $('#new_post').on('submit', function(event){
     event.preventDefault();
     var form = $(this);
@@ -16,6 +18,7 @@ $(document).ready(function(){
     var body = $('#post_body').val();
     this.reset();
 
+    //post request 
     $.ajax({
       // url: form.attr('action'),
       // method: form.attr('method'),
@@ -29,6 +32,8 @@ $(document).ready(function(){
       }, //data
       dataType: "json",
       success: function(data) {
+          console.log("Hello SUCCESS ADDED HANDLER!");
+
         console.log(data);
         var span = $('.post_1');
         var post =  "<li>"+data.title+" - " + data.body + '<button class="delete" type="submit">' + 'Delete</button>' + '<button class="show" type="submit">' + 'Show </button>'+'</li>';
@@ -41,30 +46,38 @@ $(document).ready(function(){
     }); // AJAX CLOSURE
   }); // new post submit event closure
 
-      // $('.delete').on('click', function(event) {     //button delete class
-      //  // event.preventDefault();
-      // var parent = $(this).parent();
-      // console.log(parent.attr('id'));
-      //   $.ajax({
-      //     type: 'get',
-      //     url: 'posts/delete', 
-      //     data: 'ajax=' + parent.attr('id') + '&delete=',
-      //   });        
-        
-      //   parent.fadeOut(500,function(){
-      //   parent.remove();
-        
-      //   });
-      // });
-  
+  console.log("Hello Loading!");
+  console.log($('.delete'));
+
+  //deletes item on click
+  $('.post_1').on('click', '.delete', function(event) {     //button delete class
+  event.preventDefault();
+  var parent = $(this).parent();
+  console.log(parent.attr('id'));
+  console.log("Hello DELETE HANDLER!");
+
+    $.ajax({
+      type: 'get',
+      url: 'posts/delete', 
+      data: 'ajax=' + parent.attr('id') + '&delete=',
+    });        
+    
+    parent.fadeOut(500,function(){
+    parent.remove();
+    });
+
+  });
 
 
-  var deletePost = function(event){                //defines var set to remove function ($this)
-    $(this).parents(".post_1").remove();             // removes $this delegation 
-    // PUT AJAX IN HERE
-  };
+  // var deletePost = function(event){                //defines var set to remove function ($this)
+  //   $(this).parent(".post_1").remove();             // removes $this delegation
+  //   // $(this).parent("ul").remove(); 
+  //   // $(this).remove(); 
+  //   // PUT AJAX IN HERE
+  // };
   
-  $(".post_1").on("click", ".delete", deletePost);
+  // // $(".post_1").on("click", ".delete", deletePost);
+  // $(".delete").on("click", ".delete", deletePost);
 
   // var showPost = function(event){                //defines var set to remove function ($this)
   //   var id = $(this).parents.id;             // removes $this delegation 
@@ -102,4 +115,5 @@ $(document).ready(function(){
 
   // }); // new post submit event closure
 
+  loadPosts();
 }); //document ready closure
