fa0694cf0a32d5052341ce3b065034a9056278ef
flanagan <flanagan9@gmail.com>
2014-05-27 12:40:31 -0700

    models set up

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,26 @@ ActiveRecord::Schema.define(version: 20140527183244) do
   create_table "comments", force: true do |t|
     t.string   "title"
     t.integer  "post_id"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.text     "type"
+    t.integer  "user_id"
+    t.integer  "picture_id"
+    t.date     "date"
+    t.time     "time"
+    t.float    "duration"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.boolean  "cash_only?"
+    t.text     "images"
+    t.text     "description"
+    t.float    "search_radius"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
